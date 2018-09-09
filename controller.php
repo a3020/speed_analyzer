42016f8051a8cc716fad69453ca07a77c04fe16b
a3020 <a3020@fastmail.com>
2018-09-09 10:45:43 +0200

    Bump version / update changelog

--- controller.php
+++ controller.php
@@ -12,7 +12,7 @@ final class Controller extends Package
 {
     protected $pkgHandle = 'speed_analyzer';
     protected $appVersionRequired = '8.2.1';
-    protected $pkgVersion = '1.2.1';
+    protected $pkgVersion = '1.2.2';
     protected $pkgAutoloaderRegistries = [
         'src/SpeedAnalyzer' => '\A3020\SpeedAnalyzer',
     ];
